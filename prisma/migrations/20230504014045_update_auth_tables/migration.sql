9851e672935ee886fb99743f2e3daf2dc8bc1eae
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2023-05-03 22:48:32 -0300

    Feature: Finalizando Adapter do Prisma

---
+++ prisma/migrations/20230504014045_update_auth_tables/migration.sql
@@ -0,0 +1,75 @@
+/*
+  Warnings:
+
+  - You are about to drop the `verification_requests` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the column `updated_at` on the `users` table. All the data in the column will be lost.
+  - You are about to drop the column `access_token` on the `sessions` table. All the data in the column will be lost.
+  - You are about to drop the column `created_at` on the `sessions` table. All the data in the column will be lost.
+  - You are about to drop the column `updated_at` on the `sessions` table. All the data in the column will be lost.
+  - You are about to drop the column `access_token_expires` on the `accounts` table. All the data in the column will be lost.
+  - You are about to drop the column `created_at` on the `accounts` table. All the data in the column will be lost.
+  - You are about to drop the column `provider_id` on the `accounts` table. All the data in the column will be lost.
+  - You are about to drop the column `provider_type` on the `accounts` table. All the data in the column will be lost.
+  - You are about to drop the column `updated_at` on the `accounts` table. All the data in the column will be lost.
+  - Added the required column `provider` to the `accounts` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `type` to the `accounts` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropIndex
+DROP INDEX "verification_requests_identifier_token_key";
+
+-- DropIndex
+DROP INDEX "verification_requests_token_key";
+
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "verification_requests";
+PRAGMA foreign_keys=on;
+
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_users" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "username" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT,
+    "avatar_url" TEXT,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+INSERT INTO "new_users" ("avatar_url", "created_at", "email", "id", "name", "username") SELECT "avatar_url", "created_at", "email", "id", "name", "username" FROM "users";
+DROP TABLE "users";
+ALTER TABLE "new_users" RENAME TO "users";
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+CREATE TABLE "new_sessions" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "session_token" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "expires" DATETIME NOT NULL,
+    CONSTRAINT "sessions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO "new_sessions" ("expires", "id", "session_token", "user_id") SELECT "expires", "id", "session_token", "user_id" FROM "sessions";
+DROP TABLE "sessions";
+ALTER TABLE "new_sessions" RENAME TO "sessions";
+CREATE UNIQUE INDEX "sessions_session_token_key" ON "sessions"("session_token");
+CREATE TABLE "new_accounts" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "user_id" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "provider" TEXT NOT NULL,
+    "provider_account_id" TEXT NOT NULL,
+    "refresh_token" TEXT,
+    "access_token" TEXT,
+    "expires_at" INTEGER,
+    "token_type" TEXT,
+    "scope" TEXT,
+    "id_token" TEXT,
+    "session_state" TEXT,
+    CONSTRAINT "accounts_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO "new_accounts" ("access_token", "id", "provider_account_id", "refresh_token", "user_id") SELECT "access_token", "id", "provider_account_id", "refresh_token", "user_id" FROM "accounts";
+DROP TABLE "accounts";
+ALTER TABLE "new_accounts" RENAME TO "accounts";
+CREATE UNIQUE INDEX "accounts_provider_provider_account_id_key" ON "accounts"("provider", "provider_account_id");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
